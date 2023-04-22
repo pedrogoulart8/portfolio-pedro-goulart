4d0cfd14d74106498541a4c1805f58eebf8b01d5
My Name <pedromgoulart@hotmail.com>
2023-04-21 21:33:09 -0300

    Subindo novo projeto

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -9,12 +9,14 @@ import cadastro from '../img/sitecadastro.png'
 import todolist from '../img/todolist.png'
 import filmes from '../img/filmes1.png'
 import secretportfolio from '../img/secretportfolio.png'
+import miniblog from '../img/miniblog.png'
 
 
 
 
 const projects = [
 
+    { id: "blog", name: "Mini Blog", image: miniblog, link: "https://pedrogoulart8.github.io/miniblog-react/" },
     { id: "academia", name: "Academia HardCore", image: academia, link: "https://pedrogoulart8.github.io/Academia-Hardcore/" },
     { id: "login", name: "Tela Login", image: login, link: "https://pedrogoulart8.github.io/Tela-de-Login-com-POO/" },
     { id: "filmes", name: "Movies Library", image: filmes, link: "https://pedro-goulart-api-filmes-react.vercel.app/" },  
